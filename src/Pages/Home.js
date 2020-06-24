1f5c28f63bbcafa55401ec9dc367c3e2c59ff1d3
sony-nurdianto <sonynurdianto445@gmail.com>
2020-06-25 01:43:54 +0700

    add swiper done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,57 @@
-import React, { useEffect} from "react"
+import React, {useEffect, useRef} from "react"
 import {Link} from "react-router-dom"
+import Swiper from 'swiper';
+import 'swiper/css/swiper.min.css'
+import 'swiper/js/swiper.min.js'
 import Aos from "aos"
 import "aos/dist/aos.css"
 import "./Home.css"
 import C1 from "./images/C1.jpg"
-// import C2 from "./images/Bank_icon.png"
+import C2 from "./images/Bank_icon.png"
 import B from "./images/google-play-badge.svg"
 
 const Home = () => {
+
+    const swiper = useRef(null)
+    useEffect(() => {
+        swiper.current = new Swiper(".swiper-container", {
+            // effect: 'coverflow',
+            // slidesPerView: 1,
+            loop: true,
+      loopFillGroupWithBlank: true,
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+    //   coverflowEffect: {
+    //     rotate: 50,
+    //     stretch: 10,
+    //     depth: 100,
+    //     modifier: 1,
+    //     slideShadows: false,
+    //   },
+        
+      spaceBetween: 10,
+      // init: false,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+            }
+        })
+    }, [])
 
     useEffect(() => {
         Aos.init({duration: 500})
@@ -86,8 +130,52 @@ const Home = () => {
                     <h1 className="fitur-name">Bank Pilihan</h1>
                 </span>
 
-                
+                <div className="swiper-container" data-aos="zoom-in" data-aos-anchor-placement="top-center">
 
+                    <div className="swiper-wrapper">
+
+                        <div
+                            className="card shadow swiper-slide">
+                            <img className="card-img-top" src={C2} alt="bank"/>
+                            <div className="card-body">
+                                <p className="card-text">BCA</p>
+                            </div>
+                        </div>
+                        <div
+                            className="card shadow swiper-slide">
+                            <img className="card-img-top" src={C2} alt="bank"/>
+                            <div className="card-body">
+                                <p className="card-text">Bni</p>
+                            </div>
+                        </div>
+                        <div
+                            className="card shadow swiper-slide">
+                            <img className="card-img-top" src={C2} alt="bank"/>
+                            <div className="card-body">
+                                <p className="card-text">Mandiri</p>
+                            </div>
+                        </div>
+                        <div
+                            className="card shadow swiper-slide"
+                            style={{
+                            width: '1rem'
+                        }}>
+                            <img className="card-img-top" src={C2} alt="bank"/>
+                            <div className="card-body">
+                                <p className="card-text">Mandiri</p>
+                            </div>
+                        </div>
+                        <div
+                            className="card shadow swiper-slide">
+                            <img className="card-img-top" src={C2} alt="bank"/>
+                            <div className="card-body">
+                                <p className="card-text">Mandiri</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="swiper-pagination"></div>
+                </div>
             </section>
         </div>
     )
