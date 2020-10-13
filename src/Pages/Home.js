34a61f9ef3765676f8a5c8be12bb644e3320bfba
muhammad akbar syahwana <>
2020-10-13 14:54:50 +0700

    add anchor link to content at navbar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,50 +1,43 @@
-import React from "react"
-import About from "../components/About/Syariah"
-import Landing from "../components/Landing/index"
-import Artikel from "../components/artikel/index"
+import React from "react";
+import About from "../components/About/Syariah";
+import Landing from "../components/Landing/index";
+import Artikel from "../components/artikel/index";
 // import Fitur from "../components/Fitur/index"
-import Bank from "../components/Perbandingan/index"
-import Form from "../components/Form/index"
-import Footer from "../components/Footer/index"
-import Video from "../components/videos/index"
-import "./Home.css"
-
-
-
+import Bank from "../components/Perbandingan/index";
+import Form from "../components/Form/index";
+import Footer from "../components/Footer/index";
+import Video from "../components/videos/index";
+import "./Home.css";
 
 const Home = () => {
-
-    return (
-        <div>
-            <section>
-                <Landing />
-            </section>
-            <section >
-                <About />
-            </section>
-            <section>
-                <Video />
-            </section>
-            <section>
-                <Artikel />
-            </section>
-            {/* <section>
+  return (
+    <div>
+      <section>
+        <Landing />
+      </section>
+      <section id={"tentang"}>
+        <About />
+      </section>
+      <section>
+        <Video />
+      </section>
+      <section id={"artikel"}>
+        <Artikel />
+      </section>
+      {/* <section>
                 <Fitur />
             </section> */}
-            <section >
-                <Bank />
-            </section>
-            <section>
-                <Form />
-            </section>
-            <section>
-                <Footer />
-            </section>
-        </div >
-    )
+      <section id={"bank"}>
+        <Bank />
+      </section>
+      <section id={"kontak"}>
+        <Form />
+      </section>
+      <section>
+        <Footer />
+      </section>
+    </div>
+  );
+};
 
-}
-
-export default Home
-
-
+export default Home;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,31 +1,52 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import { Navbar, Nav } from "react-bootstrap"
-import "./Navbar.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import { Navbar, Nav } from "react-bootstrap";
+import "./Navbar.css";
 // import "../../style.css"
 
-
 const navbar = () => {
+  return (
+    <Navbar
+      className="animate__animated animate__fadeIn"
+      style={{ animationDuration: "2s", animationDelay: "1s" }}
+      collapseOnSelect
+      expand="lg"
+      bg="light"
+      variant="light"
+      sticky="top"
+    >
+      <Navbar.Brand>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <strong className="span">
+            <span className="brand1">
+              B<span className="brand2">e</span>li Baik
+            </span>
+          </strong>
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <HashLink className="nav-link" to="/#tentang">
+            <strong>Tentang</strong>
+          </HashLink>
+          <HashLink className="nav-link" to="/#bank">
+            <strong>Fitur & Bank Pilihan</strong>
+          </HashLink>
+          {/* <Link className="nav-link" to="/artikel">
+            <strong>Testimoni</strong>
+          </Link> */}
+          <HashLink className="nav-link" to="/#artikel">
+            <strong>Artikel</strong>
+          </HashLink>
+          <HashLink className="nav-link" to="/#kontak">
+            <strong>Kontak</strong>
+          </HashLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-    return (
-
-        <Navbar className="animate__animated animate__fadeIn" style={{ animationDuration: "2s", animationDelay: "1s" }} collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
-            <Navbar.Brand><Link to="/" style={{ textDecoration: "none" }}><strong className="span"><span className="brand1">B<span className="brand2">e</span>li Baik</span></strong></Link></Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <Link className="nav-link" to="/tentang"><strong>Tentang</strong></Link>
-                    <Link className="nav-link" to="/artikel"><strong>Fitur & Bank Pilihan</strong></Link>
-                    <Link className="nav-link" to="/artikel"><strong>Testimoni</strong></Link>
-                    <Link className="nav-link" to="/artikel"><strong>Kontak</strong></Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
-
-}
-
-
-
-
-export default navbar
+export default navbar;
